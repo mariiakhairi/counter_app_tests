d98901dc2157884f8c9488179388533574a5f94d
mariiakhairi <mariia_khairi@inbox.ru>
2023-12-12 00:12:40 +0300

    adding configuration

---
+++ cypress/e2e/test_counter.cy.js
@@ -0,0 +1,11 @@
+describe("Test Scenarios", () => {
+  it("Increment and Decrement Tests", () => {
+    cy.visit("https://example.cypress.io");
+  });
+  it("Reset Test", () => {
+    cy.visit("https://example.cypress.io");
+  });
+  it("Reset Test", () => {
+    cy.visit("https://example.cypress.io");
+  });
+});
